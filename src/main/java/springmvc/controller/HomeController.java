ce9c8fa5ec109d590be6d0b8039321a4d22a99ac
mithu <mithu@192.168.1.27>
2024-06-19 13:46:31 +0530

    first springmvc project completed

---
+++ src/main/java/springmvc/controller/HomeController.java
@@ -0,0 +1,14 @@
+package springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/home")
+	public String home() {
+		System.out.println("Hello this is home url");
+		return "index";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Home_Page</title>
+</head>
+<body>
+	<h1>This is home page</h1>
+	<h2>Called by home controller</h2>
+	<h3>URL/home</h3>
+</body>
+</html>
